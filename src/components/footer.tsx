76b3e90fac1972b5d350fa38d7446288ec9732b0
Issac Liu <issac@matchatea.dev>
2025-12-13 22:29:42 -0500

    feat: update Footer component styles and structure for improved layout and icon paths

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,22 +2,34 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="w-full bg-gray-900 py-6 pt-12 text-white">
+    <footer className="w-full bg-[#15162c] py-6 pt-12 text-white">
       <div className="container mx-auto px-4 text-center">
-        <p className="flex flex-col items-center justify-center gap-2 text-sm">
+        <div className="flex flex-col items-center justify-center gap-2 text-sm">
           &copy; {new Date().getFullYear()} The ByteKnights - FTC Team 27705.
           All rights reserved.
-          <a href="https://instagram.com/team27705">
-            <Image
-              src="/instagram.svg"
-              width={32}
-              height={32}
-              alt="Instagram"
-              className="ml-2 inline-block"
-            />
-          </a>
+          <div>
+            Follow us on
+            <a href="https://instagram.com/team27705">
+              <Image
+                src="/icons/instagram.svg"
+                width={24}
+                height={24}
+                alt="Instagram"
+                className="ml-2 inline-block"
+              />
+            </a>
+            <a href="https://github.com/Team27705">
+              <Image
+                src="/icons/github.svg"
+                width={24}
+                height={24}
+                alt="GitHub"
+                className="ml-2 inline-block"
+              />
+            </a>
+          </div>
           Built by Issac Liu
-        </p>
+        </div>
       </div>
     </footer>
   );
